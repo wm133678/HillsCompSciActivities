8aff4c0ab2e545cfc12bd6159091f6b29c81bbe2
wm133678 <71493631+wm133678@users.noreply.github.com>
2020-12-15 14:48:29 +0000

    Update Battlebots.cs

--- Battlebots.cs
+++ Battlebots.cs
@@ -11,7 +11,7 @@ namespace BattleBots
 
             static void displayMenu()
             {
-                Console.WriteLine("\nWelcome to Battle Bots!\n\n");
+                Console.WriteLine("\nWelcome to Battle Boats!\n\n");
                 Console.WriteLine("Please enter your menu option:");
                 Console.WriteLine("1) Start a new game\n2) Resume Game\n3) Display Instructions\n4) Quit the game\n\n");
                 int menuInput = Convert.ToInt32(Console.ReadLine());
@@ -40,15 +40,19 @@ namespace BattleBots
                     createGrid(playerFleet);
                     displayGrid(playerFleet);
                     boatPlacement(playerFleet);
-   
+
 
                 }
 
                 static void boatPlacement(char[,] playerFleet)
                 {
-                    Console.WriteLine("\n--------------------------------------------------------------------------\nPlease enter the coordinate of where you want to place your boat (E.g b7)\n--------------------------------------------------------------------------\n");
-                    string boatPlacement = Console.ReadLine();
-                    Console.WriteLine(boatPlacement);
+                    Console.WriteLine("\n--------------------------------------------------------------------------\nPlease enter the first letter of the coordinate of where you want to place your boat (E.g b)\n--------------------------------------------------------------------------\n");
+                    //string boatPlacement = Console.ReadLine();
+                    // Console.WriteLine(boatPlacement);
+                    string one = Console.ReadLine();
+
+                    Console.WriteLine("\n--------------------------------------------------------------------------\nPlease enter the first letter of the coordinate of where you want to place your boat (E.g b)\n--------------------------------------------------------------------------\n");
+                    int y = Convert.ToInt32(Console.ReadLine());
                     // Edit player fleet for boat placement
                     // b7
                     Console.Write(" \n   a b c d e f g h");
@@ -58,24 +62,46 @@ namespace BattleBots
                         Console.Write(row + "  ");
                         for (int col = 0; col < playerFleet.GetLength(1); col++)
                         {
-                            char firsttt = boatPlacement[0];
-                            var x = char.ToUpper(firsttt) - 'A' + 1; //01
+                            // int y = boatPlacement[1];
+
+                            // char firsttt = boatPlacement[0];
+                            char oo = char.Parse(one);
+                            var x = Char.ToUpper(oo) - 'A' + 1; //01
+                            //Console.WriteLine(x);
+                            // Outputs 2 for B
                             // x is letter corresponding to column
-                            int y = boatPlacement[1];
-                            int rowcol = x + y;
-                            int rowcoll = col + row;
-                            if (rowcol == rowcoll)
+                            //Console.WriteLine(y);
+                            //Console.WriteLine(x);
+                            //Console.WriteLine(y);
+                            //Console.WriteLine(row);
+                            //Console.WriteLine(col);
+                            /* 2
+                             * 55
+                             * 7
+                             * 7
+                             * 
+                             * 
+                             * 
+                             */
+
+                            // if x is equal to column number replace with B
+
+                            int column = col + 1;
+                            if (x == column && y == row)
                             {
                                 //place boat
                                 Console.Write(playerFleet[row, col].ToString() + 'B');
+
                             }
                             else
                             {
                                 Console.Write(playerFleet[row, col].ToString() + ' ');
 
                             }
+
                         }
                     }
+
 
                 }
 
@@ -88,25 +114,25 @@ namespace BattleBots
                         Console.Write(row + "  ");
                         for (int col = 0; col < playerFleet.GetLength(1); col++)
                         {
-                            
+
                             Console.Write(playerFleet[row, col].ToString() + ' ');
                         }
                     }
                     Console.WriteLine("\n");
-                    
+
                 }
 
                 static void createGrid(char[,] playerFleet)
                 {
-                    
+
                     for (int row = 0; row < playerFleet.GetLength(0); row++)
                     {
                         for (int col = 0; col < playerFleet.GetLength(1); col++)
                         {
-                            playerFleet[row, col] = 'b';
+                            playerFleet[row, col] = '+';
                         }
                     }
-                    
+
                 }
 
                 static void resumeGame()
